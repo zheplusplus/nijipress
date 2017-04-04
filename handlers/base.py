640823c18f46d617b4a42e1b491d3a2b99eeffd1
Neuron Teckid <lene13@gmail.com>
2017-04-04 10:40:57 +0900

    Add files via upload

--- handlers/base.py
+++ handlers/base.py
@@ -1,4 +1,5 @@
 from google.appengine.ext import webapp
+
 import render
 
 class BaseView(webapp.RequestHandler):
@@ -24,3 +25,55 @@ def raise_not_found(handler):
 def raise_forbidden(handler):
     handler.error(403)
     render.put_page(handler, 'forbidden.html', dict())
+
+all_routes = []
+
+def Request(object):
+    def __init__(self, handler):
+        self._handler = handler
+        self._req = handler.request
+
+    def get(self, key, value=None):
+        return self._req.get(key, value)
+
+    def contains(self, key):
+        return key in self._req.arguments()
+
+    def get_of_type(self, key, tp):
+        try:
+            return tp(self._req.get(key))
+        except ValueError:
+            return tp()
+
+    def put_page(self, template_file, template_values):
+        render.put_page(self._handler, template_file, template_value)
+
+    def error_page(self, status_code, template_file):
+        self.error(status_code)
+        render.put_page(self, 'errors/' + template_file, {})
+
+    def raise_not_found(self):
+        self.error_page(404, 'not-found.html')
+
+    def raise_method_not_allowed(self):
+        self.error_page(405, 'method-not-allowed.html')
+
+    def raise_no_auth(self):
+        self.error_page(401, 'no-auth.html')
+
+    def raise_forbidden(self):
+        self.error_page(403, 'forbidden.html')
+
+def get(url):
+    def wrapper(f):
+        class GetHandler(webapp.RequestHandler):
+            def get(self):
+                return f(Request(self))
+
+            def post(self):
+                self.error(405)
+                render.put_page(self, 'errors/method-not-allowed.html', {})
+
+        all_routes.append((url, GetHandler))
+        return GetHandler
+    return wrapper
