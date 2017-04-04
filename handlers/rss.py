c1bbb21c9066bfc1e1f7d5a6fe12a1de9456c268
Neuron Teckid <lene13@gmail.com>
2017-04-04 11:29:20 +0900

    use annotation

--- handlers/rss.py
+++ handlers/rss.py
@@ -1,7 +1,8 @@
+from urlparse import urlparse
+
 import base
 import utils.escape
 import models
-from urlparse import urlparse
 
 RSS_ITEMS_COUNT = 16
 
@@ -14,13 +15,10 @@ def rss_post(post, url):
 def rss_posts(origin_posts, url):
     return map(lambda p: rss_post(p, url), origin_posts)
 
-def make_rss(view):
-    url = urlparse(view.request.url)
-    return view.put_page('feed.xml', {
+@base.get('/rss')
+def make_rss(request):
+    url = urlparse(request.url())
+    request.put_page('feed.xml', {
         'site_link': url.scheme + '://' + url.netloc,
         'posts': rss_posts(models.post.fetch(0, RSS_ITEMS_COUNT), url),
     })
-
-class Build(base.BaseView):
-    def get(self):
-        return make_rss(self)
--- handlers/sitemap.py
+++ handlers/sitemap.py
@@ -1,13 +1,13 @@
-import base
-import models
 from urlparse import urlparse
 
+import base
+import models
 
-class Build(base.BaseView):
-    def get(self):
-        url = urlparse(self.request.url)
-        self.put_page('sitemap.xml', {
-            'site_link': url.scheme + '://' + url.netloc,
-            'tags': models.tag.sort_by_count(),
-            'posts_ids': models.post.posts_ids(),
-        })
+@base.get('/sitemap.*')
+def get(request):
+    url = urlparse(request.url())
+    request.put_page('sitemap.xml', {
+        'site_link': url.scheme + '://' + url.netloc,
+        'tags': models.tag.sort_by_count(),
+        'posts_ids': models.post.posts_ids(),
+    })
